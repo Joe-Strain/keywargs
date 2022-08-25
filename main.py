edb56c99e2633b2a9869762e43f8eb7cf3d57571
Joe-Strain <51381738+Joe-Strain@users.noreply.github.com>
2022-08-25 22:47:36 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Short script to multiply any number of INTEGERS from the command line and return the total
+
+Usage: python main.py a b c...
+where a, b, c... are all integers
+
+"""
+
+
+import sys
+
+
+def multiply(**kwargs):
+    # set total to 1 outside the loops
+    total = 1
+    # iterate through the dictionary "kwargs"
+    for key, value in kwargs.items():
+        # iterate over the dictionary values
+        for j in value:
+            # only expect integer values from command line, use float otherwise
+            total *= int(j)
+    print(total)
+
+
+# call multiply with kwargs set to a list from index 1 onwards
+# index 0 is the files name
+if __name__ == '__main__':
+    multiply(kwargs=sys.argv[1:])
